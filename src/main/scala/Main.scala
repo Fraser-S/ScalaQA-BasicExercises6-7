248aa02853a2d7bf2c3135fdec590027e34e1f64
Fraser <frasers@hotmail.co.uk>
2017-06-05 13:51:19 +0000

    Added A function that combines two strings, and replaces a character with a new one.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,8 +7,14 @@ object Main {
     println(text.takeRight(amount))
   }
 
+  def concatenateStringsAndSwapCharacters(text1 : String, text2 : String, oldChar : Char, newChar : Char): Unit ={
+    var concatenatedString : String = text1.concat(text2)
+    var replacedString : String = concatenatedString.replace(oldChar, newChar)
+    println(replacedString)
+  }
+
   def main(args : Array[String]) : Unit ={
     displayEndOfStringCharacters("Hello", 3)
-
+    concatenateStringsAndSwapCharacters("Ha", "llo", 'a', 'e')
   }
 }
